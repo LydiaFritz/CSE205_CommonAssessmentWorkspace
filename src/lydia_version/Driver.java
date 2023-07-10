433eb003110363dafc6a333b81e7d993b463fa1e
Lydia <Lydia.Fritz@gcu.edu>
2023-07-10 13:16:19 -0700

    complete re-organization.  trying to implement full game first

--- src/lydia_version/Driver.java
+++ src/lydia_version/Driver.java
@@ -1,9 +1,15 @@
 package lydia_version;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args) throws FileNotFoundException {
-		Squardle s = new Squardle();
+		
+		Squardle sq = new Squardle("input.in");
+		
 	}
 }
--- src/lydia_version/Squardle.java
+++ src/lydia_version/Squardle.java
@@ -1,30 +1,101 @@
 package lydia_version;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Squardle {
-	
-	//inner position class
-	class Position{
-		
-		public int column;		
+
+	// static class variables
+	public static Scanner fin;
+
+	// inner position class
+	class Position {
+
+		public int column;
 		public int row;
+
+		Position(int c, int r) {
+			column = c;
+			row = r;
+		}
+
+		@Override
+		public String toString() {
+			return "[col:" + column + " row:" + row + "]";
+		}
 		
-		Position(int c, int r){column = c; row = r;}		
+		
 	}
-	
-	//Squardle instance variables
+
+	// Squardle instance variables
 	private int taskNum;
 	private ArrayList<String> regularWords;
 	private ArrayList<String> bonusWords;
 	private String[][] grid;
-	private Position [] positions;
-	
-	public Squardle() throws FileNotFoundException {
-		InputOutputValidate.getInput("input.in");
-		taskNum = InputOutputValidate.getTaskNum();
-		System.out.println("Task num: " + taskNum);
+	private ArrayList<Position> positions;
+
+	public Squardle(String fileName) throws FileNotFoundException {
+		fin = new Scanner(new File(fileName));
+		regularWords = new ArrayList<String>();
+		bonusWords = new ArrayList<String>();
+		getInput();
 	}
-	
+
+	private void getInput() {
+		String str = "";
+		while (fin.hasNextLine())
+			str += fin.nextLine();
+
+		// str has entire input file
+		String[] substrings = str.split(" # ");
+
+		// validation
+		//substrings[0] is taskNum
+		try {
+			taskNum = Integer.parseInt(substrings[0]);
+		} catch (InputMismatchException mismatch) {
+			System.out.println(mismatch.getLocalizedMessage());
+		}
+		
+		//get words
+		//assuming valid format (word, type)
+		//substrings[1] is word list
+		String words[] = substrings[1].split(" ");
+		//words[0] - word
+		//words[1] - type
+		for(int i = 0; i < words.length-1; i+=2) {
+			if(words[i+1].equals("R"))
+				this.regularWords.add(words[i]);
+			else
+				this.bonusWords.add(words[i]);
+		}
+		
+		//substrings[2] is grid size
+		int sz = -1;
+		try {
+			sz = Integer.parseInt(substrings[2]);
+		} catch (InputMismatchException mismatch) {
+			System.out.println(mismatch.getLocalizedMessage());
+		}
+		grid = new String[sz][sz];
+		
+		//substrings[3] is the letters in the grid
+		String letters[] = substrings[3].split(" ");
+				
+		int index = 0;
+		for(int col = 0; col < grid.length; col++) {
+			for(int row = 0; row < grid.length; row++) {
+				grid[col][row] = letters[index++];
+			}
+		}
+		
+		//substrings[4] is the list of moves
+		//moves are separated by &
+		String [] moves = substrings[4].split("& ");
+		//each element of moves is a sequence of moves to be evaluated
+	}
+
 }
