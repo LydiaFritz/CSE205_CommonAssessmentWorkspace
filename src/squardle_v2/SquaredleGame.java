39b194dbea517262b7571f2a0cc94d4b1c49f8a6
Lydia <Lydia.Fritz@gcu.edu>
2023-07-23 15:36:08 -0700

    added cell class

--- src/squardle_v2/SquaredleGame.java
+++ src/squardle_v2/SquaredleGame.java
@@ -12,5 +12,18 @@ public class SquaredleGame {
 }
 
 class Cell{
+	public int row, col;
+	public boolean visited;
+	public String value;
 	
+	public Cell(){
+		row = col = -1;
+		visited = false;
+		value = "?";
+	}
+	
+	public String toString() {
+		String str = value;
+		return str;
+	}
 }
